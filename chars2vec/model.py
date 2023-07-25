6355af4c5777542c02d872fc467b0b9c865aac40
David Westfall <david@jin-westfall.localdomain>
2023-07-25 12:38:47 -0400

    Updated imports to match newer tensorflow version

--- chars2vec/model.py
+++ chars2vec/model.py
@@ -101,8 +101,8 @@ class Chars2Vec:
 
             x_2.append(np.array(emb_list_2))
 
-        x_1_pad_seq = tf.keras.preprocessing.sequence.pad_sequences(x_1)
-        x_2_pad_seq = tf.keras.preprocessing.sequence.pad_sequences(x_2)
+        x_1_pad_seq = tf.keras.utils.pad_sequences(x_1)
+        x_2_pad_seq = tf.keras.utils.pad_sequences(x_2)
         
         self.model.fit([x_1_pad_seq, x_2_pad_seq], targets,
                     batch_size=batch_size, epochs=max_epochs,
@@ -149,7 +149,7 @@ class Chars2Vec:
 
                 list_of_embeddings.append(np.array(current_embedding))
 
-            embeddings_pad_seq = tf.keras.preprocessing.sequence.pad_sequences(list_of_embeddings, maxlen=maxlen_padseq)
+            embeddings_pad_seq = tf.keras.utils.pad_sequences(list_of_embeddings, maxlen=maxlen_padseq)
             new_words_vectors = self.embedding_model(embeddings_pad_seq)
 
             for i in range(len(new_words)):
